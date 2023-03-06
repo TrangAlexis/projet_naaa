2806da741426c16c462ff8868412cbe0630cff98
TrangAlexis <trang.alexis@hotmail.fr>
2023-03-06 18:26:56 +0100

    Add java project

---
+++ appli_naaa/src/appli_naaa/app/theTestZone.java
@@ -0,0 +1,9 @@
+package appli_naaa.app;
+
+public class theTestZone {
+	public static void main(String[] args) {
+		String s="abcd";
+		System.out.println(s.substring(0,2)+" "+s.substring(2,4));
+	}
+
+}
