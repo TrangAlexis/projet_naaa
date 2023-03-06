069e0d584cdbe07e7521fa437288a655144fd929
TrangAlexis <trang.alexis@hotmail.fr>
2023-03-07 00:01:14 +0100

    removed duplicate

--- appli_naaa/src/appli_naaa/app/theTestZone.java
+++
@@ -1,9 +0,0 @@
-package appli_naaa.app;
-
-public class theTestZone {
-	public static void main(String[] args) {
-		String s="abcd";
-		System.out.println(s.substring(0,2)+" "+s.substring(2,4));
-	}
-
-}
