4eed1130b81d2a3ea2fa0ec95927fa142f22c031
TrangAlexis <trang.alexis@hotmail.fr>
2023-04-03 17:36:48 +0200

    add clients services and tests

--- appli_naa_jpa/src/main/java/appli_sport_jpa/repositories/ClientRepository.java
+++ appli_naa_jpa/src/main/java/appli_sport_jpa/repositories/ClientRepository.java
@@ -1,5 +1,9 @@
 package appli_sport_jpa.repositories;
 
-public interface ClientRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import appli_sport_jpa.entities.Client;
+
+public interface ClientRepository extends JpaRepository<Client, Long>{
 
 }
--- appli_naa_jpa/src/main/java/appli_sport_jpa/services/ClientServices.java
+++ appli_naa_jpa/src/main/java/appli_sport_jpa/services/ClientServices.java
@@ -1,5 +1,52 @@
 package appli_sport_jpa.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import appli_sport_jpa.entities.Client;
+import appli_sport_jpa.exceptions.ClientException;
+import appli_sport_jpa.repositories.ClientRepository;
+
+@Service
 public class ClientServices {
+
+	@Autowired
+	private ClientRepository clientRepository;
+	
+	public void save(Client client) {
+		if (client.getLogin()==null || client.getLogin().isBlank()) {
+			throw new ClientException("Client sans login");
+		}
+		if (client.getEmail()==null || client.getEmail().isBlank()) {
+			throw new ClientException("Client sans e-mail");
+		}
+		clientRepository.save(client);
+	}
+
+	
+	public void deleteById(Long id) {
+		Client checkClient = this.findById(id);
+		clientRepository.deleteById(id);
+	}
+	
+	public void delete(Client client) {
+		this.deleteById(client.getId());
+	}
+		
+	public Client findById(Long id) {
+		return clientRepository.findById(id).orElseThrow(() -> {
+			throw new ClientException("ID manquante dans la base de données de clients: "+id);
+		});
+	}
+	
+	public List<Client> findAll(){
+		List<Client> lClients = clientRepository.findAll();
+		if (lClients.isEmpty()) {
+			throw new ClientException("La base de données de clients est vide");
+		}
+		return lClients;
+	}
 
 }
